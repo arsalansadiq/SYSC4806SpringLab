44369cf8d555447da626da698d597e9bd27fcb6f
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2019-02-26 15:52:31 -0500

    fixing the addressBook

--- src/main/java/hello/AddressBookController.java
+++ src/main/java/hello/AddressBookController.java
@@ -3,10 +3,7 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AddressBookController {
@@ -18,24 +15,30 @@ public class AddressBookController {
     BuddyInfoRepository bud;
 
 
-    @RequestMapping("/createAD")
-    @ResponseBody
-    public AddressBook createAddress(@RequestParam(value="name", defaultValue="12345") int name){
-        ad = new AddressBook();
-        ad.setId(name);
-        repo.save(ad);
-        return ad;
+//    @RequestMapping("/createAD")
+//    @ResponseBody
+//    public AddressBook createAddress(@RequestParam(value="name", defaultValue="12345") int name){
+//        ad = new AddressBook();
+//        ad.setId(name);
+//        repo.save(ad);
+//        return ad;
+//
+//    }
 
+    @GetMapping("/createAD")
+    public String createAddressBook(Model model) {
+        model.addAttribute("AddressBook", new AddressBook());
+        return "AddressBook";
     }
-    @RequestMapping("/createBuddy")
-    @ResponseBody
-    public BuddyInfo createBuddy(@RequestParam(value = "name") String name, @RequestParam(value = "address") String address,@RequestParam(value = "number") String number){
-        BuddyInfo buddy = new BuddyInfo(name,address,number);
-        ad.addBuddy(buddy);
-        bud.save(buddy);
-        repo.save(ad);
-        return buddy;
-    }
+//    @RequestMapping("/createBuddy")
+//    @ResponseBody
+//    public BuddyInfo createBuddy(@RequestParam(value = "name") String name, @RequestParam(value = "address") String address,@RequestParam(value = "number") String number){
+//        BuddyInfo buddy = new BuddyInfo(name,address,number);
+//        ad.addBuddy(buddy);
+//        bud.save(buddy);
+//        repo.save(ad);
+//        return buddy;
+//    }
     @RequestMapping("/Display")
     public String Display(Model model){
         model.addAttribute("address",repo.findAll());
